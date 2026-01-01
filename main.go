f824399f83f16ef5f33e81b09ce759d62df9199d
kembo <asininefatuity@gmail.com>
2026-01-01 13:27:30 +0300

    declare and initialize checkpoint

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import "fmt"
 
-func PrintMessage(message string) {
+func PrintMessage(message string) (test string) {
 	fmt.Println(message)
+	return message
 }
 func main() {
-	PrintMessage("Hi Eva, bride to be!")
+	var a string = PrintMessage("Hi Eva, bride to be!")
+	fmt.Println(a)
 }
