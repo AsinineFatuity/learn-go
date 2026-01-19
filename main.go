82d1631ee2845347fab46848ed7cd98c96c0159c
kembo <asininefatuity@gmail.com>
2026-01-19 17:56:27 +0300

    feat: added constants example with learnings on typed contsnts

--- main.go
+++ main.go
@@ -6,7 +6,14 @@ func PrintMessage(message string) (test string) {
 	fmt.Println(message)
 	return message
 }
+
+func ConstantsExample() {
+	const PI float64 = 3.14
+	var n int = 6
+	fmt.Println(float64(n+5) * PI)
+}
 func main() {
 	a := PrintMessage("Hi Eva, bride to be!")
 	fmt.Println(a)
+	ConstantsExample()
 }
