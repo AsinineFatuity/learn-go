f446708638eb26750456fc58f48992bb450de87e
kembo <asininefatuity@gmail.com>
2026-01-19 18:00:51 +0300

    feat: declared enumerators

--- main.go
+++ main.go
@@ -11,6 +11,13 @@ func ConstantsExample() {
 	const PI float64 = 3.14
 	var n int = 6
 	fmt.Println(float64(n+5) * PI)
+	// enumeration example
+	const (
+		PYTHON     = 0
+		TYPESCRIPT = 1
+		GOLANG     = 2
+	)
+	fmt.Println(PYTHON)
 }
 func main() {
 	a := PrintMessage("Hi Eva, bride to be!")
