7df29634c1523224243b5ae3f6c4ccfe7835a0dc
kembo <asininefatuity@gmail.com>
2026-01-26 07:01:07 +0300

    refactor: removed reference and made factors into constants

--- variables.go
+++ variables.go
@@ -2,27 +2,25 @@ package main
 
 import "fmt"
 
+const (
+	Factor1 = 5
+	Factor2 = 10
+)
+
 func OptimalVariables() {
-	// local variable
-	var sum int
-	{
-		// Block level local variable
-		count := 5
-		sum = scaleValue(&count, 2)
-		fmt.Println("Sum:", sum)
-	}
-	total := sum
-	{
-		count := 10
-		sum = scaleValue(&count, 3)
-		fmt.Println("Sum:", sum)
-	}
-	total += sum
+
+	sum1 := scaleValue(Factor1, 2)
+	fmt.Println("Sum:", sum1)
+
+	sum2 := scaleValue(Factor2, 3)
+	fmt.Println("Sum:", sum2)
+
+	total := sum1 + sum2
 	fmt.Println("Total:", total)
 }
 
-func scaleValue(count *int, factor int) int {
+func scaleValue(count int, factor int) int {
 	// Accessing local variable
-	scaledValue := *count * factor
+	scaledValue := count * factor
 	return scaledValue
 }
